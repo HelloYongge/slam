eb89f3c03afac2da4963e0a31e619b703249488d
yong <1193521476@qq.com>
2021-10-22 17:33:57 +0800

    create

---
+++ ch2/build/CMakeFiles/libHello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libHello.dir/src/hello.cpp.o"
+  "liblibHello.a"
+  "liblibHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch2/main.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+int main(){
+    SayHello();  
+    return 0;
+}
